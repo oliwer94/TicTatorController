78740f615c676ce378926a62b5e572e61b732d4f
oliwer94 <oliwer94@gmail.com>
2016-05-23 17:25:55 +0200

    GET user from UserService POST login credentials to UserService

--- src/main/java/com/jofa/simpleuser/SimpleUser.java
+++ src/main/java/com/jofa/simpleuser/SimpleUser.java
@@ -3,14 +3,16 @@ package com.jofa.simpleuser;
 public class SimpleUser {
 	private String username;
 	private String password;
+	private boolean admin;
 	
 	public SimpleUser() {
 		super();
 	}
-	public SimpleUser(String username, String password) {
+	public SimpleUser(String username, String password,boolean admin) {
 		super();
 		this.username = username;
 		this.password = password;
+		this.admin = admin;
 	}
 	public String getUsername() {
 		return username;
@@ -23,5 +25,11 @@ public class SimpleUser {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public boolean getAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
 	}
 }
